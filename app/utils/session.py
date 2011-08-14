f4478f8a75bdb5f1ecec6c676b84115e08bf34f9
Bjørn Uhre Arnhotlz <rugkiks@gmail.com>
2011-08-14 19:26:39 +0200

    Added expire to session, and removed debug msgs

--- app/utils/session.py
+++ app/utils/session.py
@@ -7,10 +7,10 @@ class InvalidCookieException(Exception):
     pass
 
 class Session(object):
-    def __init__(self, id):
+    def __init__(self, id, expires):
         self.id = id
         self.is_init = False
-        self.changed = False
+        self.expires = expires
     
     def init(self):
         if self.is_init:
@@ -28,19 +28,22 @@ class Session(object):
     
     def load_session(self):
         try:
-            self.doc = list(db().view("session/by_id", key=self.id, include_docs=True))[0].doc
+            doc = list(db().view("session/by_id", key=self.id, include_docs=True))[0].doc
         except IndexError:
             raise InvalidCookieException()
+
+        # Expire time on session
+        if (dateutils.now() - dateutils.fromtuple(doc["date"])).total_seconds() > self.expires:
+            raise InvalidCookieException()
+        
+        doc["date"] = dateutils.totuple(self.date)
+        self.doc = doc
         
     def new_session(self):
         self.doc = {"type": "session", "data":{}, "date": dateutils.totuple(self.date)}
     
     def save(self):
-#        if not self.changed:
-#            return
-
         self.id, self.doc["_rev"] = db().save(self.doc)
-        print self.id
     
     def get(self, *args, **kwargs):
         self.init()
@@ -48,10 +51,8 @@ class Session(object):
     
     def __setitem__(self, *args, **kwargs):
         self.init()
-        self.changed = True
         return self.doc["data"].__setitem__(*args,**kwargs)
     
     def set_cookie(self, response):
         if self.is_init:
-            print "Jeg bliver kørt"
             response.set_cookie("session", self.id, max_age=31536000)

230ccd84bf7fdc603d5c12139adeddb2bd6dbd79
Bjørn Uhre Arnholtz <rugkiks@gmail.com>
2011-08-14 14:43:56 +0200

    Fixed login function if "rkg" doesn't exists

--- app/model/user.py
+++ app/model/user.py
@@ -3,7 +3,10 @@ import app.utils.pam as pam
 import grp
 
 def authenticate(username, password):
-    if not username in grp.getgrnam("rkg").gr_mem:
+    try:
+        if not username in grp.getgrnam("rkg").gr_mem:
+            return False
+    except KeyError:
         return False
     return pam.authenticate(username, password)
 
